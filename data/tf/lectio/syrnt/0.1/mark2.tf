596ecb9d6eb69ac74f27ecbf9163197296f34e46
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2019-01-16 23:12:51 +0100

    lectionary data

---
+++ data/tf/lectio/syrnt/0.1/mark2.tf
@@ -0,0 +1,9 @@
+@node
+@converter=Dirk Roorda
+@description=lectionary start mark (alternative language) associated to verses (| separated ln:mark values)
+@editor=Geert Jan Veldman
+@name=lectionary data for the Peshitta (NT)
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2019-01-16T21:24:47Z
+
